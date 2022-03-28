5013b250d97ddf0699df494bf065367dd6c00684
Cysmi <cnb10100@gmail.com>
2022-03-28 17:08:50 -0400

    finished about and nav components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import Nav from './components/Nav';
 import About from './components/About';
 
-
 function App() {
+
   return (
     <div>
+      <Nav />
       <main>
-        <About></About>
+          <About />        
       </main>
     </div>
   );
-};
+}
 
 export default App;
